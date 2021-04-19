093ffc59f75471c0158cb06e2f0b86496a429b14
mafoloEmmanuel <mafoloemmanuel@gmail.com>
2021-04-19 12:06:32 +0200

    commits

--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -1,8 +1,3 @@
-//get a reference to the calculate button
-
-//get a reference to the billTotal element
-
-//get a reference to the billString
 
 //create the function that will be called when the calculate button is pressed
 //  * this function should read the string value entered - split it on a comma.
@@ -11,3 +6,35 @@
 //  * once done looping over all the entries - display the total onto the screen in the billTotal element
 
 //link the function to a click event on the calculate button
+//get a reference to the calculate button
+var billStringElement = document.querySelector(".billString");
+//get a reference to the billTotal element
+var calculateBtnElement = document.querySelector(".calculateBtn");
+//get a reference to the billString
+var billTotalElement = document.querySelector(".billTotal");
+
+// add event listener
+
+calculateBtnElement.addEventListener('click', function calculateBtnClicked() {
+    function totalPhoneBill(){
+        var billString = billStringField.value;
+        var isPhoneBill = billString.split(', ');
+          
+          var isTotalBill=0;
+          for(var i=0;i<isPhoneBill.length;i++){
+          var isBill= isPhoneBill[i].trim();
+            //console.log(isBill);
+           if(isBill == 'call'){
+           isTotalBill += 2.75;
+           } else if(isBill == 'sms'){
+           isTotalBill += 0.65
+           }
+        }
+          //console.log("R" + isTotalBill.toFixed(2));
+           return "R" + isTotalBill.toFixed(2);// rounds off to the 2nd decimal place
+       
+        }
+       
+        var roundedBillTotal = totalPhoneBill(billString);
+    billTotalElement.innerHTML = roundedBillTotal;
+    });
