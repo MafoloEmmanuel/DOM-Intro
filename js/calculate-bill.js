11b48424e949cd6e6099e910c1990d149ee56d4c
mafoloEmmanuel <mafoloemmanuel@gmail.com>
2021-04-19 16:36:57 +0200

    working

--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -1,39 +1,34 @@
 
-//create the function that will be called when the calculate button is pressed
-//  * this function should read the string value entered - split it on a comma.
-//  * loop over all the entries in the the resulting list
-//  * check if it is a call or an sms and add the right amount to the overall total
-//  * once done looping over all the entries - display the total onto the screen in the billTotal element
-
-//link the function to a click event on the calculate button
-//get a reference to the calculate button
 var billStringElement = document.querySelector(".billString");
-//get a reference to the billTotal element
 var calculateBtnElement = document.querySelector(".calculateBtn");
-//get a reference to the billString
 var billTotalElement = document.querySelector(".billTotal");
 
-// add event listener
-
-calculateBtnElement.addEventListener('click', function calculateBtnClicked(billString) {
-   
-        // get the string entered in the textArea
-        //split the string
-        var billItems = billString.split(",");
-        // a variable for the total phone bill.
-        var billTotal = 0;
-        //loop over all the bill items
-        for (var i=0;i<billItems.length;i++){
-            var billItem = billItems[i].trim();
-            if (billItem === "call"){
-                billTotal += 2.75;
-            }
-            else if (billItem === "sms"){
-                billTotal += 0.75;
-            }
+function totalPhoneBill(billString){
+    var isPhoneBill = billString.split(', ');
+      var isTotalBill=0;
+      for(var i=0;i<isPhoneBill.length;i++){
+      var isBill= isPhoneBill[i].trim();
+       if(isBill == 'call'){
+       isTotalBill += 2.75;
+       } else if(isBill == 'sms'){
+       isTotalBill += 0.65
+       }
+    }
+       return isTotalBill.toFixed(2);
+    }
+    function calculateBtnClicked(){
+        var billString= billStringElement.value;
+        var roundedBillTotal = totalPhoneBill(billString);
+        billTotalElement.innerHTML= roundedBillTotal;
+        var istotal= roundedBillTotal;
+        if(istotal>= 30.00){
+            billTotalElement.classList.replace(billTotalElement.className, "danger");
+        } else if(istotal>=20.00){
+            billTotalElement.classList.replace(billTotalElement.className, "warning");
+        } else {
+            billTotalElement.classList.replace(billTotalElement.className, "billTotal");
         }
-        
-        //round to two decimals
-        var roundedBillTotal = billTotal.toFixed(2);
-        billTotalElement.innerHTML = roundedBillTotal;
-    });
+
+    }
+
+calculateBtnElement.addEventListener('click',  calculateBtnClicked)
